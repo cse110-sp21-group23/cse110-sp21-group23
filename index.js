416007600c657147f0611479deb3612ff97eb27d
Daniel Huynh <danielhuynh97@gmail.com>
2021-05-09 16:16:09 -0700

    Added JSDoc (Automated Documentation)

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {*} test 
+ * @param {*} name 
+ * @param {*} variable 
+ * @returns a test object
+ */
+function testFunction(test, name, variable) {
+	return {
+		test: test,
+		name: name,
+		variable: variable,
+	}
+}
