d2c9934164d4cafd0763458c130580517bd5b45e
Caroline Xiong <carolinexiong@Carolines-MBP-2.lan>
2021-05-13 13:30:44 -0700

    refactored component using function

--- source/frontend/src/js/components/pages/weekly-component.js
+++ source/frontend/src/js/components/pages/weekly-component.js
@@ -1,14 +1,11 @@
-var coll = document.getElementsByClassName("collapsible");
+function expandComponent(e) {
+  
+  var content = e.nextElementSibling;
 
-for (var i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    }
-    else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+  if (content.style.maxHeight) {
+    content.style.maxHeight = null;
+  }
+  else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
 }
