15c41c010646580602bdca0550954382aefca9c1
Daniel Huynh <danielhuynh97@gmail.com>
2021-05-12 18:27:01 -0700

    Upload of Login Page HTML,CSS, and JS

---
+++ source/frontend/src/js/components/login-component.js
@@ -0,0 +1,32 @@
+
+function signInFunction() {
+
+	var email = document.getElementById('username-input').value;
+	var password = document.getElementById('password-input').value;
+
+	var req = JSON.stringify({
+        "email": email,
+        "password": password
+    });
+
+    console.log(email + '\n' + password);
+    //return request.get(req);
+
+	
+}
+
+function signUpFunction() {
+
+	var email = document.getElementById('username-signup-input').value;
+    var password = document.getElementById('password-signup-input').value;
+    var passwordconfirm = document.getElementById('password-signup-confirm').value;
+
+    //Check if both passwords match
+    console.log(email + ' ' + password + ' ' + (password === passwordconfirm));
+
+	var req = JSON.stringify({
+	  "email": email,
+	  "password": password
+	});
+	
+}
