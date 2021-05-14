23dbdfe77622817fc62ab90f6fca911d467801b8
kentiscool <utomo.kentjonathan@gmail.com>
2021-05-14 09:22:02 -0700

    cors error 3

--- source/server/app.js
+++ source/server/app.js
@@ -2,7 +2,6 @@
 require('dotenv').config({ path: './.env' });
 const db = require('./app/lib/pg');
 const express = require('express');
-var cors = require('cors')
 const userRouter = require('./app/api/user')
 var bodyParser = require('body-parser')
 
@@ -12,14 +11,22 @@ const PORT = process.env.PORT || 8080;
 // App
 const app = express();
 app.use(bodyParser.json())
-app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
