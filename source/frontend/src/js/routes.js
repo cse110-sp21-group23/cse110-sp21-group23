9faf12a0b8f6d0720da4e07bac0c960afd1b889f
Chris Y <55182050+46chris@users.noreply.github.com>
2021-05-20 00:09:35 -0700

    Update source/frontend/src/js/routes.js
    Co-authored-by: Daniel Huynh <52263601+dhuynh97@users.noreply.github.com>

--- source/frontend/src/js/routes.js
+++ source/frontend/src/js/routes.js
@@ -1,6 +1,6 @@
 import { DefaultPage } from './components/pages/default'
 import { LoginPage } from './components/pages/login'
-import { DailyPage } from './components/pages/dailyPage'
+import { DailyPage } from "./components/pages/dailyPage"
 
 export const routes = [
     {title: 'index', path: '', component: DefaultPage},
