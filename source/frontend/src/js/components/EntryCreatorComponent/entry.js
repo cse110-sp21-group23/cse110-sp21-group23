acfe2db2cdf4ccd99b7961a195ba83f3caa1086f
MichaelFu512 <michaelfu512@gmail.com>
2021-05-30 03:14:18 -0700

    Refactored edit bullet into entry.js with modal

--- source/frontend/src/js/components/EntryCreatorComponent/entry.js
+++ source/frontend/src/js/components/EntryCreatorComponent/entry.js
@@ -21,7 +21,33 @@ export default class Entry extends HTMLElement{
                 <p id="content"></p><br>
                 <img src="" alt="" class="entry-image"></img> <br>
                 <audio src="" class="entry-audio"></audio>
-            </li>`
+            </li>
+            <div id="myModal" class="modal">
+            <div class="modal-content">
+              <div class = "modal-header"> 
+                <span class="close" id = "close">&times;</span>
+                <h4 class = "modal-title"> Edit bullet </h4>
+              </div>
+              <form id="edit">
+                <ul>
+                  <input type="text" id="modal-words" placeholder="Your entry">
+                  <div id="radioEdit">
+                    <input type="radio" name="entryTypeEdit" id="task" value="task">
+                    <label for="task">Task</label>
+                    <input type="radio" name="entryTypeEdit" id="event" value="event"> 
+                    <label for="event">Event </label>
+                    <input type="radio" name="entryTypeEdit" id="note" value="note"> 
+                    <label for="note">Note </label> 
+                  </div>
+                  <button id="editButton"> Confirm Edit </button>
+                  <button id="doneButton"> Finish Bullet </button>
+                </ul>
+              </form>
+            </div> 
+          </div>
+              
+          </ul>
+        </div>`
 
         //Add styling for bullet points 
         let style = document.createElement('style'); 
@@ -44,6 +70,63 @@ export default class Entry extends HTMLElement{
         img{ 
             height: 300px; 
             width: auto; 
+        }
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+        
+        /* Modal Content/Box */
+        .modal-content {
+            background-color: #fefefe;
+            margin: 15% auto; /* 15% from the top and centered */
+            padding: 30px;
+            border: 5px solid #888;
+            width: 90%; /* Could be more or less, depending on screen size */
+            height: 25%;
+        }
+        #modal-words {
+            padding: 10px; 
+            width: 50vw; 
+            box-sizing: border-box; 
+            font-size: 15pt;
+        }
+        [contenteditable] {
+            outline: 2px solid;
+        }
+        /* The Close Button */
+        .close {
+            color: #aaa;
+            float: right;
+            font-size: 50px;
+            font-weight: bold;
+        }
+        
+        .close:hover,
+        .close:focus {
+            color: black;
+            text-decoration: none;
+            cursor: pointer;
+        }
+        
+        #editButton {
+            margin-top:20px;
+            padding: 10px;
+        }
+        #doneButton {
+            margin-top:20px;
+            padding: 10px;
+        }
+        .modal-title {
+            text-align: center;
         }`;
 
         //Attach shadow 
@@ -64,13 +147,11 @@ export default class Entry extends HTMLElement{
         //Set type and content of entry component 
         shadow.querySelector("#type").setAttribute("class", entry.type);
         shadow.querySelector("#content").innerHTML = entry.content; 
-        
         //Set necesary image content if the src isn't null
         if (entry.image.src != null) { 
             entryImage.setAttribute("src", entry.image.src); 
             entryImage.setAttribute("alt", entry.image.alt); 
         }
-
         //Set audio if applicable
         if (entry.audio != null){ 
             let entryAudio = shadow.querySelector(".entry-audio"); 
@@ -183,6 +264,95 @@ export default class Entry extends HTMLElement{
         this.addEventListener('dragleave', this.handleDragLeave);
         this.addEventListener('drop', this.handleDrop);
         this.addEventListener('dragend', this.handleDragEnd);
+    }
+
+    connectedCallback(){ 
+        this.render(); 
+    }
+    
+    render(){ 
+
+        const shadow = this.shadowRoot;
+
+        //Sets up modal
+        var modal = shadow.getElementById("myModal");
+        var span = shadow.getElementById("close");
+
+        //Entry part
+        let entry = this.shadowRoot.querySelector("#type");
+        var content = entry.children;
+
+        //Strikethrough button
+        var strike = shadow.getElementById("doneButton");
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        } 
+
+        //on doubleclick on entry for edit cause single click is drag&drop
+        entry.addEventListener("dblclick", () => {
+            //shows modal
+            modal.style.display = "block";
+
+            //changes modal textbox to match entry's content
+            shadow.getElementById("modal-words").value = content[0].textContent;
+            
+            //edit button from modal
+            var edit = shadow.getElementById("editButton");
+            
+            //Can't use addEventListener, it gets upset
+            edit.onclick = function(event) {
+                event.preventDefault();
+
+                //changes entry's content and bullet type
+                content[0].textContent = shadow.getElementById("modal-words").value;
+                let choices = shadow.getElementById("radioEdit").querySelectorAll("input[name='entryTypeEdit']"); 
+                for (const choice of choices) { 
+                    if (choice.checked){ 
+                        console.log("choice = " + choice.value);
+                        entry.className = choice.value;
+                        choice.checked = false;
+                    }
+                }
+                
+                //closes modal and fixes finish bullet button text
+                modal.style.display = "none";
+                strike.firstChild.nodeValue = "Finish Bullet";
+            }
+            
+            //Makes sure finish bullet button says the right text
+            if(content[0].innerHTML.startsWith("<del>")) {
+                strike.firstChild.nodeValue = "Unfinish Bullet";
+            }
+            else {
+                strike.firstChild.nodeValue = "Finish Bullet";
+            }
+
+            //adds strikethrough through entry
+            strike.onclick = function(event) {
+                event.preventDefault();
+                
+                //holds strikethrough text
+                var strikeT;
+    
+                //removes strikethrough and changes strike button txt
+                if(content[0].innerHTML.startsWith("<del>")) {
+                    strikeT = content[0].innerHTML.replace("<del>",'');
+                    strikeT = strikeT.replace("</del>", '');
+                    strike.firstChild.nodeValue = "Finish Bullet";
+                }
+                //inserts strikethrough and changes strike button txt
+                else {
+                    strikeT = "<del>" + content[0].innerHTML + "</del>";
+                    strike.firstChild.nodeValue = "Unfinish Bullet";
+                }
+    
+                //changes entry content and closes modal
+                content[0].innerHTML = strikeT;
+                modal.style.display = "none";
+            }
+        });      
     }
 }
 
