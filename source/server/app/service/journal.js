07b2ba8de72fc447d0ee03d206623d05929c896f
kentiscool <utomo.kentjonathan@gmail.com>
2021-06-02 13:05:42 -0700

    done

--- source/server/app/service/journal.js
+++ source/server/app/service/journal.js
@@ -112,7 +112,12 @@ module.exports = {
 
 const cleanBulletDate = bullet => {
     const newDate = new Date(bullet.date);
-    newDate.setDate(newDate.getDate() + 1)
+    newDate.setDate(newDate.getDate() + 1);
+    
+    console.log("ori: ", bullet.date)
+    console.log("newDate: ", newDate)
+    console.log("newDate.toDateString(): ", newDate.toDateString())
+    console.log("new new date: ", new Date(newDate.toDateString()))
     return {
         ...bullet,
         date: new Date(newDate.toDateString())
