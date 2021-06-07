66b3de1de9b35a42225ab11e190483e4274ec4af
Daniel Huynh <danielhuynh97@gmail.com>
2021-06-06 18:18:24 -0700

    successfully got rid of textfield with icon

--- source/frontend/src/js/components/calendar.js
+++ source/frontend/src/js/components/calendar.js
@@ -10,18 +10,58 @@ export default class Calendar extends HTMLElement {
         const template = document.createElement('template');
 
         template.innerHTML = `
+        <style>
+        .datepicker-toggle {
+            display: inline-block;
+            position: relative;
+            width: 18px;
+            height: 19px;
+        }
+        .datepicker-toggle-button {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            background-color: black;
+            background-image: url(https://cdn4.iconfinder.com/data/icons/small-n-flat/24/calendar-512.png);
+        }
+        .datepicker-input {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+            cursor: pointer;
+            box-sizing: border-box;
+        }
+        .datepicker-input::-webkit-calendar-picker-indicator {
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+            cursor: pointer;
+        }
+        </style>
+
+
+        <span class="datepicker-toggle">
+            <span class="datepicker-toggle-button"></span>
             <input 
                 id="today" 
-                type="date" 
+                type="date"
+                class="datepicker-input" 
                 pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))" placeholder="YYYY-MM-DD"/>
+        </span>
         `
         let style = document.createElement('style');
 
         style.textContent = `
-            #description{
-                font-family: 'Lato', sans-serif;
-                font-size: 20px;
-            }
+
         `
 
         this.shadowRoot.appendChild(template.content.cloneNode(true));
