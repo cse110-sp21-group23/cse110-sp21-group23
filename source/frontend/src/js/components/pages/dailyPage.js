4b912732f5d5669e6ba358bca72623dcd45060d3
Daniel Huynh <danielhuynh97@gmail.com>
2021-06-08 19:03:53 -0700

    fixed getDateError

--- source/frontend/src/js/components/pages/dailyPage.js
+++ source/frontend/src/js/components/pages/dailyPage.js
@@ -1,5 +1,5 @@
 import { getJournals } from '../../api/journal'
-import { getJournal, setJournal } from '../../utils/localStorage'
+import { getDate, getJournal, setJournal } from '../../utils/localStorage'
 import getHeader from '../../utils/header'
 import EntryCreator from '../EntryCreatorDay/EntryCreator'
 import DatePicker from '../DatePicker'
