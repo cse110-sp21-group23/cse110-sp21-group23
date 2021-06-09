41a0aee36f36613c0d4b4ecdae3d38141a80b5da
Christopher Yoon <chris01wy@gmail.com>
2021-06-09 15:42:09 -0700

    one sec

--- source/frontend/jest-puppeteer.config.js
+++ source/frontend/jest-puppeteer.config.js
@@ -3,6 +3,9 @@ module.exports = {
   launch: {
     headless: false,
     slowMo: 500, 
-    defaultViewport: null
+    defaultViewport: null, 
+    args: [
+      "--window-size=1584,800",
+    ]
   }
 }
