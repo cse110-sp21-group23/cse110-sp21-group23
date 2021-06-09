a62827f769b83d3d4829209ed180fa234f7f17da
Christopher Yoon <chris01wy@gmail.com>
2021-06-09 02:17:33 -0700

    Continuing testing

--- source/frontend/__tests__/dndEcWeek.test.js
+++ source/frontend/__tests__/dndEcWeek.test.js
@@ -58,25 +58,45 @@ describe('E2E testing for dragging/dropping between different lists', ()=> {
         await page.$eval('#username-input', e => e.value = "e@gmail.com")
         await page.$eval('#password-input', e => e.value = "asd")
         await page.$eval('#signin-button', e => e.click())
-        await page.waitForTimeout(10000);
+        await page.waitForTimeout(1000);
 
         //Goto the weekly page 
-
-        try{
-            await page.evaluate(()=> { 
+        await page.evaluate(()=> { 
                 let weeklyButt = document.querySelector("navigation-bar").shadowRoot.querySelector(".sidebar button:nth-child(3)"); 
                 weeklyButt.click(); 
-            })
-            //let weeklyButt = await (await page.evaluateHandle(`document.querySelector("navigation-bar").shadowRoot.querySelector(".sidebar button:nth-child(3)")`)).asElement(); 
-           // await weeklyButt.click(); 
-        }
-        catch(err){ 
-            console.log(err); 
-        }
-        await page.waitForTimeout(50000); 
+        });
+        await page.waitForTimeout(1000); 
 
-        // let weekNextButt = await (await page.evaluateHandle(`document.querySelector("weekly-page").shadowRoot.querySelector("week-picker").shadowRoot.querySelector(".full-date")`)).asElement();
-        // weekNextButt.click(); 
-        // weekNextButt.click(); 
-    }, 60000); 
+        expect(page.url()).toBe(`${baseURL}/weekly`); 
+    }, 10000); 
+
+    it('Test2: Change the week three times', async()=> { 
+        //Get the first date in the range of 8
+        let currDate = await page.evaluate(()=> { 
+            let dateTitle = document.querySelector("weekly-page").shadowRoot.querySelector("week-picker").shadowRoot.querySelector("#date").innerHTML; 
+            let dateSplit = dateTitle.split(' '); 
+            let dateNew = dateSplit[0] + ' ' + dateSplit[1] + ' ' +  dateSplit[2]; 
+            return dateNew;
+        }); 
+
+        //Click on next 3 times 
+        let nextButt = await (await page.evaluateHandle(`document.querySelector("weekly-page").shadowRoot.querySelector("week-picker").shadowRoot.querySelector("#next")`)).asElement(); 
+        await nextButt.click(); 
+        await nextButt.click(); 
+        await nextButt.click(); 
+
+        //Get the first date again 
+        let dateAfter = await page.evaluate(()=> { 
+            let dateTitle = document.querySelector("weekly-page").shadowRoot.querySelector("week-picker").shadowRoot.querySelector("#date").innerHTML; 
+            let dateSplit = dateTitle.split(' '); 
+            let dateNew = dateSplit[0] + ' ' + dateSplit[1] + ' ' +  dateSplit[2]; 
+            return dateNew;
+        });   
+        expect(dateAfter).tobe(currDate); 
+    }, 30000); 
+
+    it ('Test3: Drag element from first day to third day', async()=> { 
+
+    }); 
+
 })
