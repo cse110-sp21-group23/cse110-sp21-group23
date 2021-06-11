544e398b87ff369aabe1dfdbddfe1da7d154ad59
Wen-Tian-Pineapple <74345454+Wen-Tian-Pineapple@users.noreply.github.com>
2021-06-11 01:18:36 -0700

    Update authApi.test.js

--- source/frontend/__tests__/authApi.test.js
+++ source/frontend/__tests__/authApi.test.js
@@ -1,30 +1,30 @@
 import {login, register} from '../src/js/api/user'
 
 test('Test1: Valid login', async () => {
-  const res = await login('e@gmail.com', 'asd')
+  const res = await login('e@gmail.com', 'asd');
   expect(res.token).not.toBe(null);
 });
 
 test('Test2: Login with unregistered email', async () => {
   try {
-    await login('doesnt-exist@gmail.com', 'nope')
+    await login('doesnt-exist@gmail.com', 'nope');
   } catch (err) {
-    expect(err).toBe("Email not found")
+    expect(err).toBe("Email not found");
   }
 })
 
 test('Test3: Login with wrong password', async () => {
   try {
-    await login('e@gmail.com', 'nope')
+    await login('e@gmail.com', 'nope');
   } catch (err) {
-    expect(err).toBe("Wrong password")
+    expect(err).toBe("Wrong password");
   }
 })
 
 test('Test4: Register with used email', async () => {
   try {
-    await register('e@gmail.com', 'nope')
+    await register('e@gmail.com', 'nope');
   } catch (err) {
-    expect(err).toBe("The email has already been used")
+    expect(err).toBe("The email has already been used");
   }
 })
